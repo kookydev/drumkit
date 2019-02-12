91e1673724abc86a6dc1d1cb1127a0f9459f6bd2
Code Nation 19 <codenation@CN-19.lan>
2019-02-12 14:40:09 +0000

    Initial functionality completed

--- app.js
+++ app.js
@@ -1,3 +1,27 @@
 // Play drum by keyboard press
 
 // Play music by mouse click
+
+window.addEventListener("keydown", checkKeyPressed);
+  
+let sounds = {
+  56:"openhat", 
+  66:"kick",
+  73:"boom",
+  70:"hihat",
+  71:"snare",
+  72:"tom",
+  75:"clap",
+  84:"ride"
+ };
+
+const main = document.getElementById("main");
+function checkKeyPressed (e) {
+  
+  let keyCode = e.which;
+  console.log(keyCode);
+  let sound = document.getElementById(sounds[keyCode])
+  console.log(sound);
+  sound.play();
+  
+}
