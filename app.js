e388fb39be41e97c512f4c7926a9771913b3cd14
Code Nation 19 <codenation@CN-19.lan>
2019-02-13 12:37:23 +0000

    Lunchtime Commit :)

--- app.js
+++ app.js
@@ -1,18 +1,15 @@
 // Play drum by keyboard press
-
-// Play music by mouse click
-
 window.addEventListener("keydown", checkKeyPressed);
   
 let sounds = {
   56:"/sounds/openhat.wav", 
   66:"/sounds/kick.wav",
-  73:"/sounds/boom.wav",
+  73:"/sounds/tink.wav",
   70:"/sounds/hihat.wav",
   71:"/sounds/snare.wav",
   72:"/sounds/tom.wav",
   75:"/sounds/clap.wav",
-  77:"/sounds/tink.wav",
+  77:"/sounds/boom.wav",
   84:"/sounds/ride.wav",
   86:"/sounds/openhat.wav"
  };
@@ -23,4 +20,7 @@ function checkKeyPressed (e) {
     let sound = new Audio(sounds[keyCode]);
     sound.play();
 };
+// Play music by mouse click
+
+
 
